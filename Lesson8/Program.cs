91bbb793b1e4328954cd88a8b857ba407b4f4d14
BaneleS352 <BaneleS@betsoftware.com>
2026-03-25 09:50:51 +0200

    8.1
    8.1

---
+++ Lesson8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+public class ConfigLoader
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            string path = "config.txt";
+            await File.WriteAllTextAsync(path, "theme=dark");
+
+            var loader = new ConfigLoader();
+            string config = await loader.LoadConfigAsync(path);
+
+            Console.WriteLine("Loaded config:");
+            Console.WriteLine(config);
+        }
+    }
+
+    // TODO:
+    // 1) Rename this to LoadConfigAsync
+    public class ConfigLoader
+    {
+        // 2) Change the return type to Task<string>
+        // 3) Use File.ReadAllTextAsync(path)
+        public async Task<string> LoadConfigAsync(string path)
+        {
+            return await File.ReadAllTextAsync(path);
+        }
+    }
+
+}
+
