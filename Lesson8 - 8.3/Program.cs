aed16c438a8f638082c228d8b3bef5c7776510e3
BaneleS352 <BaneleS@betsoftware.com>
2026-03-25 10:06:01 +0200

    8.3
    8.3

---
+++ Lesson8 - 8.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MiniExercise12_3_ClassifyWork
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var examples = new List<(string Work, string Classification)>
+          {
+              ("File.ReadAllTextAsync(...)", "I/O-bound"),
+              ("HttpClient.GetStringAsync(...)", "I/O-bound"),
+              ("summing 1 to 50 million in a loop", "CPU-bound"),
+              ("a database query", "I/O-bound")
+          };
+
+            Console.WriteLine("Classification results:");
+
+            foreach (var example in examples)
+            {
+                Console.WriteLine($"{example.Work} -> {example.Classification}");
+            }
+        }
+    }
+}
