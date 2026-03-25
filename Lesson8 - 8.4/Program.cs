699de04d49ed0abb3348a9359580baf08c064aa5
BaneleS352 <BaneleS@betsoftware.com>
2026-03-25 10:14:06 +0200

    12.4
    12.4

---
+++ Lesson8 - 8.4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        var http = new HttpClient();
+
+        Console.WriteLine("A) Starting download...");
+
+        // TODO: start GetStringAsync, store it in a Task<string> variable (do NOT await yet)
+        Task<string> downloadTask = http.GetStringAsync("https://example.com");
+
+        // TODO: print "B) Doing something else..."
+        Console.WriteLine("B) Doing something else...");
+        // TODO: await the task and print "C) Done!" + length
+
+        string html = await downloadTask;
+
+        Console.WriteLine("C) Done!");
+        Console.WriteLine($"Length: {html.Length}");
+    }
+}
